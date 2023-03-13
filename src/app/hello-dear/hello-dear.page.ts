33574043a598712da2c5a2639b28e3b000b2672a
StephenVivek <100684744+StephenVivek@users.noreply.github.com>
2023-03-13 19:33:06 +0530

    hello dear screen changes and routing changes

--- src/app/hello-dear/hello-dear.page.ts
+++ src/app/hello-dear/hello-dear.page.ts
@@ -8,12 +8,77 @@ import { Router, ActivatedRoute, NavigationExtras } from '@angular/router';
 })
 export class HelloDearPage implements OnInit {
 
+  signUpButtons:boolean=false;
+  individualCard:boolean=false;
+  institutionalCard:boolean=false;
+  organizationCard:boolean=false;
+
   constructor(public router:Router) { }
 
   ngOnInit() {
   }
 
-  goto_signup(){
-    this.router.navigate(['/sign-up'])
-}
+  goto_signup(sign:any){
+    if(sign=='sign'){
+      this.signUpButtons=true;
+      this.individualCard=false;
+      this.institutionalCard=false;
+      this.organizationCard=false;
+    }
+  }
+
+  individualClick(individualClick:any){
+    if(individualClick=='individual'){
+      this.individualCard=true;
+      this.signUpButtons=false;
+    }else{
+      this.individualCard=false;
+    }
+  }
+
+  institutionalClick(institutionalClick:any){
+    if(institutionalClick=='institutional'){
+      this.institutionalCard=true;
+      this.signUpButtons=false;
+      this.individualCard=false;
+    }else{
+      this.institutionalCard=false;
+    }
+  }
+
+  organizationClick(organizationClick:any){
+    if(organizationClick=='organization'){
+      this.organizationCard=true;
+      this.signUpButtons=false;
+      this.individualCard=false;
+      this.institutionalCard=false;
+    }else{
+      this.organizationCard=false;
+    }
+  }
+
+  individualRegister(){
+    this.router.navigate(['/sign-up']);
+    this.individualCard=false;
+    this.institutionalCard=false;
+    this.organizationCard=false;
+    this.signUpButtons=false;
+  }
+
+  organizationRegister(){
+    this.router.navigate(['/sign-up-organization']);
+    this.individualCard=false;
+    this.institutionalCard=false;
+    this.organizationCard=false;
+    this.signUpButtons=false;
+  }
+
+  institutionRegister(){
+    this.router.navigate(['/sign-up-institution']);
+    this.individualCard=false;
+    this.institutionalCard=false;
+    this.organizationCard=false;
+    this.signUpButtons=false;
+  }
+
 }
