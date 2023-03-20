7775156a797a0319e092c9313543a25e5fd72edd
Morrisvijay <99658370+Morrisvijay@users.noreply.github.com>
2023-03-20 22:56:06 +0530

    search bar

--- src/app/job-search/job-search.page.ts
+++ src/app/job-search/job-search.page.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-job-search',
@@ -7,10 +8,47 @@ import { Component, OnInit } from '@angular/core';
 })
 
 export class JobSearchPage implements OnInit {
-
-  constructor() { }
+  jobSearchHeadForm : FormGroup;
+  jobSearchForm : FormGroup;
+  uls :any = [];
+  constructor(private fb: FormBuilder) { }
 
   ngOnInit() {
+    this.jobSearchHeadForm = this.fb.group({
+      searchType :["talentid"],
+      searchValue :[""]
+    })
+    this.uls = document.querySelectorAll("ul");
+
+    this.uls.forEach((ul) => {
+      const resetClass = ul.parentNode.getAttribute("class");
+      const lis = ul.querySelectorAll("li");
+
+      lis.forEach((li) => {
+        li.addEventListener("click", (e) => {
+          e.preventDefault();
+          e.stopPropagation();
+          const target = e.currentTarget;
+    
+          if (
+            target.classList.contains("active") ||
+            target.classList.contains("follow")
+          ) {
+            return;
+          }
+    
+          ul.parentNode.setAttribute(
+            "class",
+            `${resetClass} ${target.getAttribute("data-where")}-style`
+          );
+    
+          lis.forEach((item) => this.clearClass(item, "active"));
+    
+          this.setClass(target, "active");
+        });
+      });
+    });
+
   }
   
   slideOpts = {
@@ -18,10 +56,20 @@ export class JobSearchPage implements OnInit {
     speed: 400
   };
 
+  search(){
+   console.log(this.jobSearchHeadForm.value); 
+  }
   selectedTab: string = 'search';
 
   setSelectedTab(tabName: string) {
     this.selectedTab = tabName;
   }
-
+  clearClass(node, className) {
+    node.classList.remove(className);
+  }
+  
+  setClass(node, className) {
+    node.classList.add(className);
+  }
+ 
 }
