af68b5d512a11d56911e3073f0eb6cd793443d09
Swathy0112 <swathyvivekanandan@gmail.com>
2023-03-22 18:53:17 +0530

    apply

--- src/app/apply-for-job/apply-for-job.page.ts
+++ src/app/apply-for-job/apply-for-job.page.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-apply-for-job',
@@ -7,7 +8,7 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ApplyForJobPage implements OnInit {
 
-  constructor() { }
+  constructor(public router: Router,) { }
 
   ngOnInit() {
   }
@@ -16,5 +17,9 @@ export class ApplyForJobPage implements OnInit {
   setSelectedTab(tabName: string) {
     this.selectedTab = tabName;
   }
+  Apply(){
 
+    this.router.navigate(['/job-details']) 
+
+  }
 }
