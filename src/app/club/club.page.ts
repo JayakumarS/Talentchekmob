39178eee8fdb0fa7ae5f03943fc93912adddf3f5
Swathy0112 <sivakumarkannan2001@gmail.com>
2023-04-24 18:14:20 +0530

    club

--- src/app/club/club.page.ts
+++ src/app/club/club.page.ts
@@ -270,7 +270,7 @@ getOrganisationList(){
          this.clubFrom.value.participatedTill =formatDate(this.clubFrom.value.participatedTill, 'dd/MM/yyyy','en-IN');          
          }
          this.clubFrom.value.currentUserId = this.userId;
-         this.clubFrom.value.clubName =  this.name; 
+         this.clubFrom.value.clubName = this.clubFrom.value.clubId;
          this.Extracurricular = this.clubFrom.value;
          console.log(` data: ${JSON.stringify(this.Extracurricular)}`);
          var saveperonalinfo = "api/auth/app/IndividualProfileDetails/saveExtracurricular";
