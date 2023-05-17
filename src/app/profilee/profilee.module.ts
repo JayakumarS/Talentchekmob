5414ab0c407c5a6ec338fbdfc623c67310aeb580
Swathy0112 <swathyvivekanandan@gmail.com>
2023-05-17 16:32:31 +0530

    General info & Skills translation

--- src/app/profilee/profilee.module.ts
+++ src/app/profilee/profilee.module.ts
@@ -7,6 +7,7 @@ import { IonicModule } from '@ionic/angular';
 import { ProfileePageRoutingModule } from './profilee-routing.module';
 
 import { ProfileePage } from './profilee.page';
+import { TranslateModule } from '@ngx-translate/core';
 
 @NgModule({
   imports: [
@@ -14,7 +15,8 @@ import { ProfileePage } from './profilee.page';
     FormsModule,
     IonicModule,
     ReactiveFormsModule,
-    ProfileePageRoutingModule
+    ProfileePageRoutingModule,
+    TranslateModule.forChild(),
   ],
   declarations: [ProfileePage]
 })
--- src/app/skill-popup/skill-popup.module.ts
+++ src/app/skill-popup/skill-popup.module.ts
@@ -7,6 +7,7 @@ import { IonicModule } from '@ionic/angular';
 import { SkillPopupPageRoutingModule } from './skill-popup-routing.module';
 
 import { SkillPopupPage } from './skill-popup.page';
+import { TranslateModule } from '@ngx-translate/core';
 
 @NgModule({
   imports: [
@@ -14,7 +15,8 @@ import { SkillPopupPage } from './skill-popup.page';
     FormsModule,
     IonicModule,
     ReactiveFormsModule,
-    SkillPopupPageRoutingModule
+    SkillPopupPageRoutingModule,
+    TranslateModule.forChild(),
   ],
   declarations: [SkillPopupPage]
 })
