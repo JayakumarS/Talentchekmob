6669ebf9b6cd0cf5dd50a340740fb32c84ab2f81
Swathy0112 <swathyvivekanandan@gmail.com>
2023-05-18 12:28:08 +0530

    Org general edit translation

--- src/app/org-profile/org-profile.module.ts
+++ src/app/org-profile/org-profile.module.ts
@@ -7,6 +7,7 @@ import { IonicModule } from '@ionic/angular';
 import { OrgProfilePageRoutingModule } from './org-profile-routing.module';
 
 import { OrgProfilePage } from './org-profile.page';
+import { TranslateModule } from '@ngx-translate/core';
 
 @NgModule({
   imports: [
@@ -14,7 +15,8 @@ import { OrgProfilePage } from './org-profile.page';
     FormsModule,
     IonicModule,
     ReactiveFormsModule,
-    OrgProfilePageRoutingModule
+    OrgProfilePageRoutingModule,
+    TranslateModule.forChild(),
   ],
   declarations: [OrgProfilePage]
 })
