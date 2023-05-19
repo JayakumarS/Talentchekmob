978ce78fd28bd8f005c16de8e530d2adafb77713
Swathy0112 <swathyvivekanandan@gmail.com>
2023-05-19 19:25:52 +0530

    Ins dashboard

--- src/app/institution-dashboard/institution-dashboard.module.ts
+++ src/app/institution-dashboard/institution-dashboard.module.ts
@@ -7,13 +7,15 @@ import { IonicModule } from '@ionic/angular';
 import { InstitutionDashboardPageRoutingModule } from './institution-dashboard-routing.module';
 
 import { InstitutionDashboardPage } from './institution-dashboard.page';
+import { TranslateModule } from '@ngx-translate/core';
 
 @NgModule({
   imports: [
     CommonModule,
     FormsModule,
     IonicModule,
-    InstitutionDashboardPageRoutingModule
+    InstitutionDashboardPageRoutingModule,
+    TranslateModule.forChild(),
   ],
   declarations: [InstitutionDashboardPage]
 })
