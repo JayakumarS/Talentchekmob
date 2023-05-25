6db3936352b765855c2f3d97f4a42c4ee31f4cbd
Swathy0112 <swathyvivekanandan@gmail.com>
2023-05-25 14:38:32 +0530

    Academic verifier translate

--- src/app/academic-verifier-details/academic-verifier-details.module.ts
+++ src/app/academic-verifier-details/academic-verifier-details.module.ts
@@ -7,6 +7,7 @@ import { IonicModule } from '@ionic/angular';
 import { AcademicVerifierDetailsPageRoutingModule } from './academic-verifier-details-routing.module';
 
 import { AcademicVerifierDetailsPage } from './academic-verifier-details.page';
+import { TranslateModule } from '@ngx-translate/core';
 
 @NgModule({
   imports: [
@@ -14,7 +15,8 @@ import { AcademicVerifierDetailsPage } from './academic-verifier-details.page';
     FormsModule,
     IonicModule,
     ReactiveFormsModule,
-    AcademicVerifierDetailsPageRoutingModule
+    AcademicVerifierDetailsPageRoutingModule,
+    TranslateModule.forChild(),
   ],
   declarations: [AcademicVerifierDetailsPage]
 })
