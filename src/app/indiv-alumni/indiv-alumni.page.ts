0b087ff70cba3a95b3939803b098c5b10e6fac2b
Swathy0112 <swathyvivekanandan@gmail.com>
2023-06-08 12:32:11 +0530

    Indiv alumni offset

--- src/app/indiv-alumni/indiv-alumni.page.ts
+++ src/app/indiv-alumni/indiv-alumni.page.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { NavigationExtras, Router } from '@angular/router';
 import { StorageService } from '../storage.service';
+import { LanguageService } from '../language.service';
 
 @Component({
   selector: 'app-indiv-alumni',
@@ -10,26 +11,65 @@ import { StorageService } from '../storage.service';
 export class IndivAlumniPage implements OnInit {
 
   userId: string;
+  talentId: string;
   alumniList:any;
-  constructor(public router:Router,public storageservice: StorageService) { }
+  mySlicedArray = [];
+  flagChange:boolean=false;
+  selectedLang: string;
+  constructor(public router:Router,public storageservice: StorageService,public languageService:LanguageService) { }
 
   ngOnInit() {
     this.getAlumniList();
+    this.selectedLang  = localStorage.getItem('selectedLang');
+    this.languageService.setLanguage(this.selectedLang);
   }
 
   
 
   getAlumniList(){
 
-    this.userId = localStorage.getItem("userId")  ; 
+    let offset = 0;
+    this.talentId = localStorage.getItem("userId")  ; 
 
-    var alumniListURL = "api/auth/app/contactGrid/orgAlumniList?talentId="+this.userId;
-    this.storageservice.getrequest(alumniListURL).subscribe(data => {
-    console.log(data);
-    if(data['Success'] == true){
-this.alumniList = data['searchList'];
+    var alumniListURL = "api/auth/app/contactGrid/orgAlumniListMob";
+    this.storageservice.getrequest(alumniListURL+ "?talentId=" + this.talentId +"&offset=" + offset).subscribe(result => { 
+    
+    console.log(result);
+    if(result['Success'] == true){
+    this.alumniList = result['searchList'];
+    this.mySlicedArray = this.alumniList;
+         console.log(this.mySlicedArray);
+        this.flagChange =true;
+        this.storageservice.dismissLoading();
     }
     });
+  }
+
+  loadMore(event){
+    let length2 = 0;
+    if(this.mySlicedArray.length != 0){
+      let length = this.mySlicedArray.length;
+      length2 = length
+      console.log(length2)
+      this.talentId = localStorage.getItem("userId")  ; 
+      var alumniListURL = "api/auth/app/contactGrid/orgAlumniListMob";
+      this.storageservice.getrequest(alumniListURL+ "?talentId=" + this.talentId +"&offset=" + length2).subscribe(result => { 
+     
+        this.alumniList = result['searchList'];
+        if(this.alumniList.length>=1){
+          this.mySlicedArray=this.mySlicedArray.concat(this.alumniList);
+         
+         this.flagChange =true;
+         this.storageservice.dismissLoading();
+         }
+         else{
+           this.flagChange=false;
+           this.storageservice.dismissLoading();
+         } 
+     }); 
+    
+      event.target.complete();
+    }
   }
 
   goto_settings(){
