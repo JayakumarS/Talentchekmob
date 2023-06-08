2131e6214444eafef4fc9d276808a96cbc337b77
Swathy0112 <swathyvivekanandan@gmail.com>
2023-06-08 16:36:12 +0530

    oni alumni list

--- src/app/oni-alumni-list/oni-alumni-list.page.ts
+++ src/app/oni-alumni-list/oni-alumni-list.page.ts
@@ -4,6 +4,7 @@ import { StorageService } from '../storage.service';
 import { FormBuilder, FormGroup } from '@angular/forms';
 import { AlertController, LoadingController, ModalController } from '@ionic/angular';
 import { ProfileViewPopupPage } from '../profile-view-popup/profile-view-popup.page';
+import { LanguageService } from '../language.service';
 
 @Component({
   selector: 'app-oni-alumni-list',
@@ -11,6 +12,7 @@ import { ProfileViewPopupPage } from '../profile-view-popup/profile-view-popup.p
   styleUrls: ['./oni-alumni-list.page.scss'],
 })
 export class OniAlumniListPage implements OnInit {
+  selectedLang: string;
 
   doRefresh(event) {
     this.ngOnInit();
@@ -27,14 +29,16 @@ export class OniAlumniListPage implements OnInit {
   currentUserId:any;
   currentUserName:any;
   title:string;
+  mySlicedArray = [];
 
   constructor(private route: ActivatedRoute, private storageservice: StorageService,private fb: FormBuilder, public modalController: ModalController,
-    public router:Router,private loadingCtrl: LoadingController,public alertController: AlertController) {
+    public router:Router,private loadingCtrl: LoadingController,public alertController: AlertController,private languageService: LanguageService) {
 
 
     this.creditPoints = localStorage.getItem("creditPoints") ;
     this.roleId = localStorage.getItem("roleId");
     this.currentUserId = localStorage.getItem("userId");
+    console.log(this.currentUserId)
     this.currentUserName = localStorage.getItem("userName");
 
     this.studentNetwork = this.fb.group({
@@ -47,13 +51,17 @@ export class OniAlumniListPage implements OnInit {
       highlightType:[""],
       highlightFlag:[""],
       designation: [""],
-      department: [""]
+      department: [""],
+      offset:[""]
     });
 
     
    }
 
   ngOnInit() {
+
+    this.selectedLang  = localStorage.getItem('selectedLang');
+    this.languageService.setLanguage(this.selectedLang);
 
     this.route.queryParams.subscribe(params => {
       if (params) {
@@ -77,17 +85,26 @@ export class OniAlumniListPage implements OnInit {
 
    }
 
-  get_StudentNetwork(){
+get_StudentNetwork(){
+  console.log(this.currentUserId)
     this.studentNetwork.value['talentId'] =this.currentUserId;
-this.storageservice.showLoading();
-    var indiRatingsCountURL = "api/auth/app/Network/getStudentNetworkList";
-  this.storageservice.get(indiRatingsCountURL,this.studentNetwork.value).subscribe(result => {
+    this.storageservice.showLoading();
 
-if(result['success'] == true) {
+    this.studentNetwork.patchValue({
+      'offset' : 0,
+      'talentId' : this.currentUserId,
+  
+    })
+
+    var indiRatingsCountURL = "api/auth/app/Network/getStudentNetworkListMob";
+    this.storageservice.get(indiRatingsCountURL,this.studentNetwork.value).subscribe(result => {
+
+  if(result['success'] == true) {
   console.log(result);
   this.storageservice.dismissLoading();
   this.studentNetworkCount = result['studentNetworkList'].lenght;
   this.commonList = result['studentNetworkList'];
+  this.mySlicedArray = this.commonList;
 }else{
   this.storageservice.dismissLoading();
 }   
@@ -95,21 +112,120 @@ if(result['success'] == true) {
 }
 
 get_CorporateNetwork(){
+  console.log(this.currentUserId)
   this.studentNetwork.value['talentId'] =this.currentUserId;
   this.storageservice.showLoading();
-  var corporateNetworkURL = "api/auth/app/Network/getCorporateNetworkList";
+  // let offset = 0;
+
+  this.studentNetwork.patchValue({
+    'offset' : 0,
+    'talentId' : this.currentUserId,
+
+  })
+
+  var corporateNetworkURL = "api/auth/app/Network/getCorporateNetworkListMob";
 
   this.storageservice.get(corporateNetworkURL,this.studentNetwork.value).subscribe(res => {
   
     console.log(res);
     if(res['success'] == true){
       this.storageservice.dismissLoading();
-      this.corporateNetworkCount = res['corporateNetworkList'].length;
+      // this.corporateNetworkCount = this.mySlicedArray.length;
       this.commonList = res['corporateNetworkList'];
+      this.mySlicedArray = this.commonList;
     }
     this.storageservice.dismissLoading();
   
   }); 
+}
+
+loadMore(event){
+  let length2 = 0;
+  
+
+
+  this.route.queryParams.subscribe(params => {
+    if (params) {
+
+      if (params != null) {
+
+        console.log(params);
+         this.title=params.title;
+        //  this.oniType=params.oniType
+      }
+    }
+  });
+ console.log(this.title)
+
+  if(this.title=='Corporate')
+  {
+    if(this.mySlicedArray.length != 0){
+      let length = this.mySlicedArray.length;
+      length2 = length
+      console.log(length2)
+      
+      this.studentNetwork.patchValue({
+        // 'talentId' : this.onitalentId,
+        'offset' : length2,
+  
+      })
+      var corporateNetworkURL = "api/auth/app/Network/getCorporateNetworkListMob";
+
+      this.storageservice.get(corporateNetworkURL,this.studentNetwork.value).subscribe(res => {
+         this.commonList = res['corporateNetworkList'];
+         
+      if(this.commonList.length>=1){
+          this.mySlicedArray=this.mySlicedArray.concat(this.commonList);
+          this.corporateNetworkCount = this.mySlicedArray.length;
+         
+         
+         this.storageservice.dismissLoading();
+         }
+         else{
+          
+           this.storageservice.dismissLoading();
+         } 
+     }); 
+    
+      event.target.complete();
+    }
+  }
+ else if(this.title=='Student')
+ {
+  if(this.mySlicedArray.length != 0){
+    let length = this.mySlicedArray.length;
+    length2 = length
+    console.log(length2)
+    // console.log(this.onitalentId)
+    this.studentNetwork.patchValue({
+      // 'talentId' : this.onitalentId,
+      'offset' : length2,
+
+    })
+
+   
+
+    var indiRatingsCountURL = "api/auth/app/Network/getStudentNetworkListMob";
+    this.storageservice.get(indiRatingsCountURL,this.studentNetwork.value).subscribe(result => {
+       this.commonList =  result['studentNetworkList'];
+       
+    if(this.commonList.length>=1){
+        this.mySlicedArray=this.mySlicedArray.concat(this.commonList);
+       
+       
+       this.storageservice.dismissLoading();
+       }
+       else{
+        
+         this.storageservice.dismissLoading();
+       } 
+   }); 
+  
+    event.target.complete();
+  }
+ }
+ 
+  
 }
 
 
